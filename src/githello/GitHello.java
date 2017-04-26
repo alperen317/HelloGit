2d12ad00d6556950b1b210320d9d5144c935c103
alperen317 <alialperen.aslan@stu.fsm.edu.tr>
2017-04-26 16:51:00 +0300

    add sout

--- src/githello/GitHello.java
+++ src/githello/GitHello.java
@@ -16,6 +16,7 @@ public class GitHello {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello git ");
     }
     
 }
